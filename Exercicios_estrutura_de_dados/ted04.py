311f4f419b533ca191841e0bade4546a14c2151c
Igor Sales <igorsales.fs@gmail.com>
2020-04-05 22:28:39 -0300

    add function handlingInputErrors()

--- Exercicios_estrutura_de_dados/ted04.py
+++ Exercicios_estrutura_de_dados/ted04.py
@@ -1,14 +1,21 @@
 class SheldonGame:
     def __init__(self, sheldonOption=None, rajOption=None):
-        self.successCases = {'pedra': ['lagarto', 'tesoura'],
+        self.successCase = {'pedra': ['lagarto', 'tesoura'],
                              'papel': ['pedra', 'spock'],
                              'tesoura': ['papel', 'lagarto'],
                              'lagarto': ['spock', 'papel'],
                              'spock': ['tesoura', 'pedra']}
-        self.attemptCounter = 0
+        self.attemptCounter = 1
         self.MAX_ATTEMPTS = 100
         self.sheldonOption = sheldonOption
         self.rajOption = rajOption
+
+    def handlingInputErrors(self):
+        if self.sheldonOption and self.rajOption not in self.successCase:
+            options = input('Entradas inválidas, Digite novamente suas opções: ')
+            self.separateOptions(options)
+            return self.handlingInputErrors()
+        return True
 
     def tieCase(self, sheldonOption, rajOption):
         if sheldonOption == rajOption:
@@ -29,6 +36,7 @@ class SheldonGame:
 
         if self.attemptCounter < self.MAX_ATTEMPTS:
 
+            self.handlingInputErrors()
             returnTieCase = self.tieCase(self.sheldonOption, self.rajOption)
 
             if returnTieCase:
@@ -36,10 +44,10 @@ class SheldonGame:
                 self.separateOptions(options)
                 return self.checkAttempts()
 
-            if self.rajOption in self.successCases[self.sheldonOption]:
+            if self.rajOption in self.successCase[self.sheldonOption]:
                 return print(f'Caso #{self.attemptCounter}: Bazinga!')
             else:
-                return print(f'Caso #{self.attemptCounter}: Raj trapaceuou!')
+                return print(f'Caso #{self.attemptCounter}: Raj trapaceou!')
         else:
             print('Você ultrapassou o máximo de tentativas!')
 
