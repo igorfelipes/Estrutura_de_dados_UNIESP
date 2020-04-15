f4276bbebd3c951df0dc93c11bed714717add37a
Igor Sales <igorsales.fs@gmail.com>
2020-04-15 20:55:45 -0300

    update

--- Exercicios_estrutura_de_dados/matrizes_exercicioE.py
+++ Exercicios_estrutura_de_dados/matrizes_exercicioE.py
@@ -1,6 +1,10 @@
 def main():
-    matriz = [[input('Digite separado por um espaço o nome, matricula e data do aluno em cada linha respectivamente: ').strip() for _ in range(3)]for _ in range(3)]
-    print(matriz)
+    matriz = [[int(input(f'Digite o valor da posição {sizeI+1} da {sizeJ+1}º linha: ')) for sizeJ in range(4)]
+              for sizeI in range(4)]
+
+    for line in matriz:
+        for value in line:
+            print(value, end='') if value > 10 else print('', end='')
 
 
 if __name__ == '__main__':
---
+++ Exercicios_estrutura_de_dados/matrizes_exercicioF.py
@@ -0,0 +1,7 @@
+def main():
+    matriz = [[input('Digite separado por um espaço o nome, matricula e data do aluno em cada linha respectivamente: ').strip() for _ in range(3)]for _ in range(3)]
+    print(matriz)
+
+
+if __name__ == '__main__':
+    main()
