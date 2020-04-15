efefabebb5c8ea30e2908f010f7d3f8197e78dce
Igor Sales <igorsales.fs@gmail.com>
2020-04-15 20:42:00 -0300

    adicionando exercicio E

--- Exercicios_estrutura_de_dados/matrizes_exercicioE.py
+++ Exercicios_estrutura_de_dados/matrizes_exercicioE.py
@@ -2,7 +2,9 @@ def main():
     matriz = [[int(input(f'Digite o valor da posição {sizeI+1} da {sizeJ+1}º linha: ')) for sizeJ in range(4)]
               for sizeI in range(4)]
 
-    print(matriz)
+    for line in matriz:
+        for value in line:
+            print(value, end='') if value > 10 else print('', end='')
 
 
 if __name__ == '__main__':
