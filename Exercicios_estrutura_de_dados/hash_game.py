96643b7c5c1421ddcfa4d78b658145965da7b29c
Igor Sales <igorsales.fs@gmail.com>
2020-04-21 23:58:08 -0300

    add file hash-game

--- Exercicios_estrutura_de_dados/hash_game.py
+++ Exercicios_estrutura_de_dados/hash_game.py
@@ -1,12 +1,25 @@
 class HashGame:
 
     def __init__(self):
-        self.array = [[], [], []]
         self.INDEX_MAX = 3
         self.player1 = []
         self.player2 = []
         self.symbolO = 'O'
         self.symbolX = 'X'
+        self.arraySymbol = ['X', 'O']
+        self.matrizPrincipal = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+        self.divisoria = '----|----|-----'
+        self.counter = 1
+        self.col = 0
+        self.line = 0
+        self.gameEnd = False
+
+    def handlerError(self):
+        pass
+
+    def convertPositions(self, value):
+        self.col = int(value[0]) - 1
+        self.line = int(value[1]) - 1
 
     def setPlayers(self):
         self.player1.append(input('Digite o nome do primeiro jogador: '))
@@ -15,8 +28,9 @@ class HashGame:
     def setSymbol(self):
         print()
         print(' ----------PLAYER 1 ------------ ')
-        option = input('1- O     2- X')
-        if option.upper() == self.symbolO:
+        print('1- O     2- X')
+        option = input('Opção: ')
+        if option.upper() == self.symbolO or option == '1':
             self.player1.append(self.symbolO)
             self.player2.append(self.symbolX)
         else:
@@ -24,14 +38,68 @@ class HashGame:
             self.player2.append(self.symbolO)
 
     def setPositions(self):
-        pass
+        while not self.gameEnd:
+            self.printGame()
+            if self.counter == 1:
+                print()
+                print('------PLAYER 1 -------')
+                print()
+                positions = input('Digite a coluna  e a linha separado por um espaço em branco:  ').split()
+                self.convertPositions(positions)
+                self.verifyPositions()
+                self.counter = 2
+                continue
+
+            if self.counter == 2:
+                print('------PLAYER 2 -------')
+                positions = input('Digite a coluna  e a linha separado por um espaço em branco:  ').split()
+                self.convertPositions(positions)
+                self.verifyPositions()
+                self.matrizPrincipal[self.line][self.col] = self.player2[1]
+                self.counter = 1
+
+    def printGame(self):
+        viewLine1 = [' ', ' ', self.matrizPrincipal[0][0], ' ', '|',
+                          ' ', ' ', self.matrizPrincipal[0][1], ' ', '|',
+                          ' ', ' ', self.matrizPrincipal[0][2], ' ']
+        viewLine2 = [' ', ' ', self.matrizPrincipal[1][0], ' ', '|',
+                          ' ', ' ', self.matrizPrincipal[1][1], ' ', '|',
+                          ' ', ' ', self.matrizPrincipal[1][2], ' ']
+        viewLine3 = [' ', ' ', self.matrizPrincipal[2][0], ' ', '|',
+                          ' ', ' ', self.matrizPrincipal[2][1], ' ', '|',
+                          ' ', ' ', self.matrizPrincipal[2][2], ' ']
+        print('\n')
+        print(''.join(viewLine1))
+        print(self.divisoria)
+        print(''.join(viewLine2))
+        print(self.divisoria)
+        print(''.join(viewLine3))
 
     def verifyPositions(self):
-        pass
+        if self.matrizPrincipal[self.line][self.col] in self.arraySymbol:
+            print('Posição já selecionada por algum jogador, tente novamente')
+            self.setPositions()
+        else:
+            self.matrizPrincipal[self.line][self.col] = self.player1[1]
+
+        for index in range(len(self.matrizPrincipal)):
+            if all(value == self.player1[1] for value in self.matrizPrincipal[index]):
+                self.printGame()
+                self.gameEnd = True
+                return print(self.player1[0], 'Venceu!')
+            elif all(value == self.player2[1] for value in self.matrizPrincipal[index]):
+                self.printGame()
+                self.gameEnd = True
+                return print(self.player2[0], 'Venceu!')
+
 
 
 def main():
-    pass
+    print('-----------------JOGO DA VELHA----------------------')
+    hashGame = HashGame()
+    hashGame.setPlayers()
+    hashGame.setSymbol()
+    hashGame.setPositions()
 
 
 if __name__ == '__main__':
