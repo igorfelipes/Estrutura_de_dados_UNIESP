fa4aa4ea7a7dd25049e3a0a1bdc725f80c8b44c7
Igor Sales <igorsales.fs@gmail.com>
2020-05-13 20:13:27 -0300

    update file pilha

--- Exercicios_estrutura_de_dados/pilha/exercicio3.py
+++ Exercicios_estrutura_de_dados/pilha/exercicio3.py
@@ -3,7 +3,7 @@ from pilha import Pilha
 def main():
     pilha = Pilha()
 
-    string = input('Digite as caracters que deseja inserir na pilha: ')
+    string = input('Digite as caracters que deseja inserir na pilha (insira todas as caracters juntas): ')
 
     [pilha.pushPilha(c)for c in string]
     print(pilha.getPilha())
